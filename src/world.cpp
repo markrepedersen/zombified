ca74a0c0f4a7676c659370f53af1c7d2a89ce5d5
Pinia Chandra <ppnia@Pinias-MBP.hitronhub.home>
2018-02-15 10:34:06 -0800

    deleted unnecessary code

--- src/world.cpp
+++ src/world.cpp
@@ -11,10 +11,6 @@
 // Same as static in c, local to compilation unit
 namespace
 {
-// const size_t MAX_TURTLES = 15;
-// const size_t MAX_FISH = 5;
-// const size_t TURTLE_DELAY_MS = 2000;
-// const size_t FISH_DELAY_MS = 5000;
 
 //TODO
 //insert constants such as max_zombies,
@@ -34,11 +30,9 @@ void glfw_err_cb(int error, const char *desc)
 
 
 World::World() :
-//m_points(0),
 m_next_arm_spawn(rand()%(1000)+500),
 m_next_leg_spawn(rand()%(1000)+500)
-// m_next_turtle_spawn(0.f),
-// m_next_fish_spawn(0.f)
+
 {
     // Seeding rng with random device
     m_rng = std::default_random_engine(std::random_device()());
@@ -106,8 +100,7 @@ bool World::init(vec2 screen)
     
     //TODO: set up music files here
     // m_background_music = Mix_LoadMUS(audio_path("music.wav"));
-    // m_salmon_dead_sound = Mix_LoadWAV(audio_path("salmon_dead.wav"));
-    // m_salmon_eat_sound = Mix_LoadWAV(audio_path("salmon_eat.wav"));
+   
 
     //TODO: check if sounds were loaded
     // if (m_background_music == nullptr || m_salmon_dead_sound == nullptr || m_salmon_eat_sound == nullptr)
@@ -121,7 +114,6 @@ bool World::init(vec2 screen)
 
     //fprintf(stderr, "Loaded music");
 
-    //m_current_speed = 1.f;
 
 
       //set game screen to resolution ratio
@@ -148,28 +140,21 @@ void World::destroy()
 
     // if (m_background_music != nullptr)
     // 	Mix_FreeMusic(m_background_music);
-    // if (m_salmon_dead_sound != nullptr)
-    // 	Mix_FreeChunk(m_salmon_dead_sound);
-    // if (m_salmon_eat_sound != nullptr)
-    // 	Mix_FreeChunk(m_salmon_eat_sound);
-
-    
+   
     //Mix_CloseAudio();
-    m_worldtexture.destroy();
-    //m_toolboxManager.destroy();
-    //m_player1.destroy();
-    //m_player2.destroy();
-    //m_water.destroy();
-    //m_freeze.destroy();
-    
+ 
     //TODO: free players, zombies, limbs, any items on the map, walls
-    // m_salmon.destroy();
+    m_worldtexture.destroy();
+    m_toolboxManager.destroy();
+    m_player1.destroy();
+    m_player2.destroy();
+    m_water.destroy();
+    m_freeze.destroy();
     for (auto& legs : m_legs)
      	legs.destroy();
     for (auto& arms : m_arms)
      	arms.destroy();
-    // m_turtles.clear();
-    // m_fish.clear();
+
     glfwDestroyWindow(m_window);
 }
 
@@ -187,73 +172,14 @@ bool World::update(float elapsed_ms)
     //zombie to player collision?
     //item to player collision?
 
-    // Checking Salmon - Turtle collisions
-    // for (const auto& turtle : m_turtles)
-    // {
-    // 	if (m_salmon.collides_with(turtle))
-    // 	{
-    // 		if (m_salmon.is_alive())
-    // 			Mix_PlayChannel(-1, m_salmon_dead_sound, 0);
-    // 		m_salmon.kill();
-    // 		break;
-    // 	}
-    // }
-
-    // Checking Salmon - Fish collisions
-    // auto fish_it = m_fish.begin();
-    // while (fish_it != m_fish.end())
-    // {
-    // 	if (m_salmon.collides_with(*fish_it))
-    // 	{
-    // 		fish_it = m_fish.erase(fish_it);
-    // 		m_salmon.light_up();
-    // 		Mix_PlayChannel(-1, m_salmon_eat_sound, 0);
-    // 		++m_points;
-    // 	}
-    // 	else
-    // 		++fish_it;
-    // }
-
+    
     //TODO: upating all entities (player, zombies, limbs, items )
-    // Updating all entities, making the turtle and fish
-    // faster based on current
     m_player1.update(elapsed_ms);
     m_player2.update(elapsed_ms);
-    // for (auto& turtle : m_turtles)
-    // 	turtle.update(elapsed_ms * m_current_speed);
-    // for (auto& fish : m_fish)
-    // 	fish.update(elapsed_ms * m_current_speed);
-
+  
     //TOOD: removing all out of screen entities, probably not needed if collision with edges of screen is
     //always checked
 
-    // // Removing out of screen turtles
-    // auto turtle_it = m_turtles.begin();
-    // while (turtle_it != m_turtles.end())
-    // {
-    // 	float w = turtle_it->get_bounding_box().x / 2;
-    // 	if (turtle_it->get_position().x + w < 0.f)
-    // 	{
-    // 		turtle_it = m_turtles.erase(turtle_it);
-    // 		continue;
-    // 	}
-
-    // 	++turtle_it;
-    // }
-
-    // // Removing out of screen fish
-    // fish_it = m_fish.begin();
-    // while (fish_it != m_fish.end())
-    // {
-    // 	float w = fish_it->get_bounding_box().x / 2;
-    // 	if (fish_it->get_position().x + w < 0.f)
-    // 	{
-    // 		fish_it = m_fish.erase(fish_it);
-    // 		continue;
-    // 	}
-
-    // 	++fish_it;
-    // }
 
     //TODO: spawn limbs, items
     m_next_arm_spawn -= elapsed_ms;
@@ -304,35 +230,6 @@ bool World::update(float elapsed_ms)
         }
     }
 
-    // // Spawning new turtles
-    // m_next_turtle_spawn -= elapsed_ms * m_current_speed;
-    // if (m_turtles.size() <= MAX_TURTLES && m_next_turtle_spawn < 0.f)
-    // {
-    // 	if (!spawn_turtle())
-    // 		return false;
-
-    // 	Turtle& new_turtle = m_turtles.back();
-
-    // 	// Setting random initial position
-    // 	new_turtle.set_position({ screen.x + 150, 50 + m_dist(m_rng) * (screen.y - 100) });
-
-    // 	// Next spawn
-    // 	m_next_turtle_spawn = (TURTLE_DELAY_MS / 2) + m_dist(m_rng) * (TURTLE_DELAY_MS/2);
-    // }
-
-    // // Spawning new fish
-    // m_next_fish_spawn -= elapsed_ms * m_current_speed;
-    // if (m_fish.size() <= MAX_FISH && m_next_fish_spawn < 0.f)
-    // {
-    // 	if (!spawn_fish())
-    // 		return false;
-    // 	Fish& new_fish = m_fish.back();
-
-    // 	new_fish.set_position({ screen.x + 150, 50 + m_dist(m_rng) *  (screen.y - 100) });
-
-    // 	m_next_fish_spawn = (FISH_DELAY_MS / 2) + m_dist(m_rng) * (FISH_DELAY_MS / 2);
-    // }
-
     if ((int)difftime( time(0), start) == m_counter)
         timer_update();
     return true;
@@ -341,6 +238,7 @@ bool World::update(float elapsed_ms)
 void World::timer_update()
 {
 
+//TODO use time elapsed instead?
     if (m_sec == 0)
     {
         m_sec = 59;
@@ -402,11 +300,6 @@ void World::draw()
         arms.draw(projection_2D);
     for (auto &legs : m_legs)
         legs.draw(projection_2D);
-    // for (auto& turtle : m_turtles)
-    // 	turtle.draw(projection_2D);
-    // for (auto& fish : m_fish)
-    // 	fish.draw(projection_2D);
-    //m_salmon.draw(projection_2D);
 
     m_toolboxManager.draw(projection_2D);
     m_player1.draw(projection_2D);
@@ -453,39 +346,11 @@ bool World::spawn_legs()
     return false;
 }
 
-//bool World::spawn_turtle()
-//{
-//	Turtle turtle;
-//	if (turtle.init())
-//	{
-//		m_turtles.emplace_back(turtle);
-//		return true;
-//	}
-//	fprintf(stderr, "Failed to spawn turtle");
-//	return false;
-//}
 
-// Creates a new fish and if successfull adds it to the list of fish
-//bool World::spawn_fish()
-//{
-//	Fish fish;
-//	if (fish.init())
-//	{
-//		m_fish.emplace_back(fish);
-//		return true;
-//	}
-//	fprintf(stderr, "Failed to spawn fish");
-//	return false;
-//}
 
 // On key callback
 void World::on_key(GLFWwindow *, int key, int, int action, int mod)
 {
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // HANDLE SALMON MOVEMENT HERE
-    // key is of 'type' GLFW_KEY_
-    // action can be GLFW_PRESS GLFW_RELEASE GLFW_REPEAT
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     if (action == GLFW_PRESS && key == GLFW_KEY_UP)
         m_player1.set_key(0, true);
@@ -542,25 +407,9 @@ void World::on_key(GLFWwindow *, int key, int, int action, int mod)
     //	if (action == GLFW_RELEASE && (mod & GLFW_MOD_SHIFT) && key == GLFW_KEY_PERIOD)
     //		m_current_speed += 0.1f;
     //
-    //	m_current_speed = fmax(0.f, m_current_speed);
 }
 
 void World::on_mouse_move(GLFWwindow *window, double xpos, double ypos)
 {
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // HANDLE SALMON ROTATION HERE
-    // xpos and ypos are relative to the top-left of the window, the salmon's
-    // default facing direction is (1, 0)
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    //	vec2 curr_pos = m_salmon.get_position();
-    //
-    //	float delta_x = curr_pos.x - xpos;
-    //	float delta_y = curr_pos.y - ypos;
-    //
-    //	float rad = atan( delta_y / delta_x );
-    //
-    //	if (delta_x > 0) rad += M_PI;
-    //
-    //	m_salmon.set_rotation(rad);
+   
 }
